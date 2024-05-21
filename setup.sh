36b8481687a81371af7ce5e3f5b88827ec03fb3a
VetleViking <123548420+VetleViking@users.noreply.github.com>
2024-05-21 10:22:38 +0200

    added setting up static IP

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# Update before running
+INTERFACE="eth0"
+STATIC_IP="192.168.1.100"
+GATEWAY="192.168.1.1"
+DNS1="8.8.8.8"
+DNS2="8.8.4.4"
+
+NETPLAN_CONFIG="/etc/netplan/01-netcfg.yaml"
+BACKUP_CONFIG="/etc/netplan/01-netcfg.yaml.bak"
+
+if [ -f "$NETPLAN_CONFIG" ]; then
+  echo "Backing up current netplan configuration to $BACKUP_CONFIG"
+  cp $NETPLAN_CONFIG $BACKUP_CONFIG
+else
+  echo "No existing netplan configuration found, creating new configuration"
+fi
+
+echo "Writing new netplan configuration"
+
+cat <<EOL > $NETPLAN_CONFIG
+network:
+  version: 2
+  ethernets:
+    $INTERFACE:
+      dhcp4: no
+      addresses:
+        - $STATIC_IP/24
+      gateway4: $GATEWAY
+      nameservers:
+        addresses:
+          - $DNS1
+          - $DNS2
+EOL
+
+echo "Applying new netplan configuration"
+netplan apply
+
+echo "Static IP configuration complete"
